fbd07b0869f7b8a2018dddb8b9897f37c0b71737
MisaelUAT <a2223410342@alumnos.uat.edu.mx>
2025-09-12 09:56:09 -0500

    Cambios en el Login Incompleto ||MH

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,16 +1,29 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Login } from './login/login';
+import { Registro } from './registro/registro';
+
+const routes: Routes = [
+  {path: 'login', component: Login, data: {background_class: 'bg_login'}},
+  {path:'',component: Login, data: {background_class: 'bg_login'}}
+]
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Login,
+    Registro
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+        RouterModule.forRoot(routes)
+  ],
+  exports:[
+    RouterModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
 import { Component, signal } from '@angular/core';
+import { Router, NavigationEnd,ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,36 @@ import { Component, signal } from '@angular/core';
 })
 export class App {
   protected readonly title = signal('Techmex_cursesnld');
+  private class_actually?: string;
+  private listClass_backgrounds: Array<string> = [
+    'bg_login'
+  ]
+
+  constructor(private router: Router, private activeRoute: ActivatedRoute){
+    this.router.events.subscribe( 
+      event=>{
+        if (event instanceof NavigationEnd){
+          let route = this.activeRoute;
+            while(route.firstChild){
+              route = route.firstChild
+            }
+            const background_class = route.snapshot.data["background_class"];
+            
+            if(this.class_actually){
+              document.body.classList.remove(this.class_actually);
+              this.class_actually = undefined;
+            } else{
+              this.listClass_backgrounds.forEach(classcss=>
+                document.body.classList.remove(classcss)
+              )
+            };
+
+            if (background_class) {
+              document.body.classList.add(background_class)
+              this.class_actually= background_class
+            }
+        }
+      }
+    )
+  }
 }
