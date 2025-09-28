1de1dccc6a4e724ba4b376e838ac56d67fcf697f
MisaelUAT <a2223410342@alumnos.uat.edu.mx>
2025-09-27 18:13:46 -0600

    Cambios en el Estilo Login y Registro ||MH

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { platformBrowser } from '@angular/platform-browser';
 import { AppModule } from './app/app-module';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 platformBrowser().bootstrapModule(AppModule, {
   ngZoneEventCoalescing: true,
 })
   .catch(err => console.error(err));
+
+
