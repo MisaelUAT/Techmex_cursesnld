3228b30dd72ef07161b2c2004b02af51a45f5ae5
MisaelUAT <a2223410342@alumnos.uat.edu.mx>
2025-09-27 22:59:58 -0500

    Animacion de login, y Configuracion de Outlet

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -5,18 +5,30 @@ import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Login } from './login/login';
 import { Registro } from './registro/registro';
+import { Inicio } from './inicio/inicio';
+import { Introduccion } from './inicio/introduccion/introduccion';
+import { Header } from './header/header';
+import { Navbar } from './navbar/navbar';
+import { Footer } from './footer/footer';
 
 const routes: Routes = [
   {path: 'login', component: Login, data: {background_class: 'bg_login'}},
   {path: 'registro', component: Registro, data: {background_class: 'bg_registro'}},
-  {path:'',component: Login, data: {background_class: 'bg_login'}}
+  {path:'',component: Login, data: {background_class: 'bg_login'}},
+  {path: 'inicio',component: Inicio},
+  {path: 'introduccion', component: Introduccion,outlet: 'inicio' }
 ]
 
 @NgModule({
   declarations: [
     App,
     Login,
-    Registro
+    Registro,
+    Inicio,
+    Introduccion,
+    Header,
+    Navbar,
+    Footer
   ],
   imports: [
     BrowserModule,
--- src/app/registro/registro.ts
+++ src/app/registro/registro.ts
@@ -7,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './registro.css'
 })
 export class Registro {
+  step_actually !: number;
+
+  ngOnInit(){
+    this.step_actually = 1;
+  }
+
+  step_slide_continue(){
+
+    this.step_actually ++;
+  }
 
 }
